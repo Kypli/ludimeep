a88a3c6117a060191b6b2521efda829376d1214b
Pierre <pierre.amboise@yahoo.fr>
2024-05-15 12:16:08 +0200

    css actu

--- assets/app.js
+++ assets/app.js
@@ -2,10 +2,11 @@
 import './modules/modules.js';
 
 // JS
-import './js/navigation/navigation.js';
 import './js/service/toaster.js';
 import './js/service/service.js';
+import './js/navigation/navigation.js';
 
 // CSS
 import './styles/app.css';
 import './styles/navigation.scss';
+// import './bootstrap.js';
